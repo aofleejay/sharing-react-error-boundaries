6ff0eab508bc654797d91abad467e300827ce9f4
kunapot <kunapot@thinknet.co.th>
2018-01-19 11:04:17 +0700

    set error message

--- src/components/BugComponent.js
+++ src/components/BugComponent.js
@@ -3,10 +3,11 @@ import Layout from './Layout'
 
 class BugComponent extends Component {
   render() {
-    throw new Error('Crashed!!!')
+    throw new Error('Cannot read property xxx of undefined.')
     return (
       <Layout>
-        <h1>Content</h1>
+        <h1>วิลเลจเวิร์คสันทนาการโบว์ลิ่ง</h1>
+        <p>รันเวย์รายชื่อ ล็อตอุปนายิกาพฤหัส คอนโทรลโพลล์ทรู โปรโมเตอร์เทรลเลอร์ ดยุก ก่อนหน้าตัวตนซัมเมอร์ เซอร์เพาเวอร์โฟน ไตรมาส นิวส์ แอดมิสชัน ซิมมอบตัววอลซ์ฮัลโลวีน ชาร์ป มิวสิคพุทธภูมิซาดิสม์ฟีดสคริปต์ แคร็กเกอร์ภูมิทัศน์สไตล์ เช็งเม้งสเตชันแคร็กเกอร์รีสอร์ท</p>
       </Layout>
     )
   }
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import Layout from './Layout'
 
 class ErrorBoundary extends Component {
-  state = { hasError: false }
+  state = { hasError: false, errorMessage: null }
 
   componentDidCatch(error, info) {
-    this.setState({ hasError: true })
+    this.setState({ hasError: true, errorMessage: error.message })
   }
 
   render() {
@@ -13,6 +13,7 @@ class ErrorBoundary extends Component {
       return (
         <Layout>
           <h1>Whoops, looks like something went wrong.</h1>
+          <p>{this.state.errorMessage}</p>
         </Layout>
       )
     }
